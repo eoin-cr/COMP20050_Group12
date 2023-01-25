d7681e2b06c3a98cc11adf1f842be83ea7dec0ca
Eoin <79023828+eoin-cr@users.noreply.github.com>
2023-01-25 23:09:15 +0000

    Cleaned up file a bit

--- src/Input.java
+++ src/Input.java
@@ -1,16 +1,21 @@
-import java.util.*;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Random;
 
 public class Input {
     public static String[] getPlayers () {
-        // note that in this method we use an arraylist as it's simpler, but
+        // Note that in this method we use an arraylist as it's simpler, but
         // we return a *String array*, which we get from the randomise method
         ArrayList<String> players = new ArrayList<>();
-        System.out.println("Enter number of players (must be 2-4)");
         Scanner in = new Scanner(System.in);
         int num = 0;
         boolean firstRun = true;
         boolean intInputted = true;
-//        String tmp;
+
+        System.out.println("Enter number of players (must be 2-4)");
+
         while (num > 4 || num < 2) {
             if (!firstRun && intInputted) {
                 System.out.println("Invalid number.  Please enter the number of players (must be between 2-4)");
@@ -18,28 +23,21 @@ public class Input {
             try {
                 num = in.nextInt();
                 intInputted = true;
-            } catch (InputMismatchException ex) {
+            } catch (InputMismatchException ex) { // catches if user doesn't enter an int
                 System.out.println("You must enter an integer!  Please try again");
-//                tmp = in.nextLine();
-                in.nextLine();
+                in.nextLine(); // clears buffer
                 intInputted = false;
             }
             firstRun = false;
         }
+
         System.out.printf("Enter the names of the %d players.  Hit enter after each name.\n", num);
-//        tmp = in.nextLine();
-        in.nextLine();
+        in.nextLine(); // clears buffer
         for (int i = 0; i < num; i++) {
             players.add(in.nextLine());
         }
-        while (players.size() > num) {
-            System.out.printf("Invalid number of players.  Remember that you selected %d players!\n", num);
-            players.clear();
-            for (int i = 0; i < num; i++) {
-                players.add(in.nextLine());
-            }
-        }
 
+        // Returns randomised String array version of the player names
         return randomisePlayers(players);
     }
 
@@ -48,9 +46,6 @@ public class Input {
         String[] randomisedPlayers = new String[players.size()];
         Arrays.fill(randomisedPlayers, "");
 
-//        for (int i = 0; i < players.size(); i++) {
-//            randomIndexes.add(new Random().nextInt(players.size()));
-//        }
         while (randomIndexes.size() < players.size()) {
             int index = new Random().nextInt(players.size());
             if (!randomIndexes.contains(index)) {
@@ -58,11 +53,14 @@ public class Input {
             }
         }
 
-//        System.out.println(randomIndexes);
+        // Random indexes now contains a list of all ints 0...num players
+        // in a random order.  We can use that to put the player names in a
+        // random order.
         int i = 0;
         for (Integer element : randomIndexes) {
             randomisedPlayers[element] = players.get(i++);
         }
+
         return randomisedPlayers;
     }
 }
