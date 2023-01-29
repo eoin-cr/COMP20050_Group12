9b434be2184bfd1e52101b65431480f5e2b70c99
Eoin <79023828+eoin-cr@users.noreply.github.com>
2023-01-29 17:57:06 +0000

    Moved tile drawing functions into output class

--- src/Output.java
+++ src/Output.java
@@ -81,7 +81,7 @@ public class Output {
     	System.out.println("The current Habitat Tile + Wildlife Token pairs up for selection are: ");
     	for (HabitatTile i : tileTokenPairs.keySet()) {
     		System.out.println("Tile: " + i + ", Token: " + tileTokenPairs.get(i));
-    		DrawTiles.printHalfTile(i);
+    		printHalfTile(i);
     	}
     	System.out.println();
 	}
@@ -89,7 +89,7 @@ public class Output {
 	public static void displayTileMap(Player player) {
 		System.out.println("Player " + player.getPlayerName() + "'s current map of tiles are: \n");
 		for (HabitatTile tile : player.getPlayerTiles()) {
-			DrawTiles.printHalfTile(tile);
+			printHalfTile(tile);
 		}
 	}
 	
@@ -103,5 +103,46 @@ public class Output {
 				Enter QUIT to quit the program.
 				""");
 //		System.out.println();
+	}
+
+
+	public static final String ANSI_RESET = "\u001B[0m";
+//    public static final String ANSI_BLACK = "\u001B[30m";
+
+	// ANSI colours to colour console text.  Indexes match with the
+	// Tile.habitats enum.
+	static String[] HabitatColours = {
+			"\u001B[32m",
+			"\u001B[36m",
+			"\u001B[34m",
+			"\u001B[37m",
+			"\u001B[33m"
+	};
+
+//    public static void PrintFullTile (HabitatTile tile) {
+//        // NOTE: only draws the first habitat colour for now
+////        System.out.println(tile.habitat1.ordinal());
+//        System.out.println(HabitatColours[tile.getHabitat1().ordinal()] +
+//                "|||| |||| |||| ||||\n" +
+//                "||||           ||||\n" +
+//                "||||           ||||\n" +
+//                "|||| |||| |||| ||||\n" +
+//                ANSI_RESET
+//                );
+//    }
+
+	// note that these printing things are all pretty primitive atm
+	public static void printHalfTile (HabitatTile tile) {
+		System.out.println(HabitatColours[tile.getHabitat1().ordinal()] +
+				"|||| |||| |||| ||||\n" +
+				"||||           ||||\n" +
+				ANSI_RESET +
+				HabitatColours[tile.getHabitat2().ordinal()] +
+				"||||           ||||\n" +
+				"|||| |||| |||| ||||\n" +
+				ANSI_RESET
+		);
+
+
 	}
 }
