5b29f7b240832684ebdb6e4f58e15959402237a1
Eoin <79023828+eoin-cr@users.noreply.github.com>
2023-01-29 19:44:06 +0000

    Changed output to match project handbook, added more sleep calls.

--- src/Output.java
+++ src/Output.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class Output {
@@ -8,21 +9,22 @@ public class Output {
 //            "/\\/\\____________/\\/\\/\\/\\____/\\/\\__________/\\/\\____________/\\/\\/\\/\\____/\\/\\____/\\/\\____/\\/\\______/\\/\\/\\/\\___        \n" +
 //            "/\\/\\__________/\\/\\____/\\/\\____/\\/\\/\\/\\____/\\/\\__________/\\/\\____/\\/\\__/\\/\\____/\\/\\____/\\/\\____/\\/\\____/\\/\\_        \n" +
 //            "/\\/\\__________/\\/\\/\\/\\/\\/\\__________/\\/\\__/\\/\\__________/\\/\\/\\/\\/\\/\\__/\\/\\____/\\/\\____/\\/\\____/\\/\\/\\/\\/\\/\\_        \n" +
-//            "__/\\/\\/\\/\\/\\__/\\/\\____/\\/\\__/\\/\\/\\/\\/\\______/\\/\\/\\/\\/\\__/\\/\\____/\\/\\__/\\/\\/\\/\\/\\____/\\/\\/\\/\\__/\\/\\____/\\/\\_        \n" +
-//            "____________________________________________________________________________________________________________\n"
+//            "__/\\/\\/\\/\\/\\__/\\/\\____/\\/\\__/\\/\\/\\/\\/\\______/\\/\\/\\/\\/\\__/\\/\\____/\\/\\__/\\/\\/\\/\\/\\____/\\/\\/\\/\\__/\\/\\____/\\/\\_        \n"
+////            "____________________________________________________________________________________________________________\n"
 //        );
 //
 //        System.out.println(
 //            "________/\\\\\\\\\\\\\\\\\\_____/\\\\\\\\\\\\\\\\\\________/\\\\\\\\\\\\\\\\\\\\\\__________/\\\\\\\\\\\\\\\\\\_____/\\\\\\\\\\\\\\\\\\_____/\\\\\\\\\\\\\\\\\\\\\\\\_____/\\\\\\\\\\\\\\\\\\\\\\_____/\\\\\\\\\\\\\\\\\\____\n" +
-//            "_____/\\\\\\////////____/\\\\\\\\\\\\\\\\\\\\\\\\\\____/\\\\\\/////////\\\\\\_____/\\\\\\////////____/\\\\\\\\\\\\\\\\\\\\\\\\\\__\\/\\\\\\////////\\\\\\__\\/////\\\\\\///____/\\\\\\\\\\\\\\\\\\\\\\\\\\__\n" +
-//            "___/\\\\\\/____________/\\\\\\/////////\\\\\\__\\//\\\\\\______\\///____/\\\\\\/____________/\\\\\\/////////\\\\\\_\\/\\\\\\______\\//\\\\\\_____\\/\\\\\\______/\\\\\\/////////\\\\\\_\n" +
-//            "__/\\\\\\_____________\\/\\\\\\_______\\/\\\\\\___\\////\\\\\\__________/\\\\\\_____________\\/\\\\\\_______\\/\\\\\\_\\/\\\\\\_______\\/\\\\\\_____\\/\\\\\\_____\\/\\\\\\_______\\/\\\\\\_\n" +
-//            "_\\/\\\\\\_____________\\/\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\______\\////\\\\\\______\\/\\\\\\_____________\\/\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\_\\/\\\\\\_______\\/\\\\\\_____\\/\\\\\\_____\\/\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\_\n" +
-//            "_\\//\\\\\\____________\\/\\\\\\/////////\\\\\\_________\\////\\\\\\___\\//\\\\\\____________\\/\\\\\\/////////\\\\\\_\\/\\\\\\_______\\/\\\\\\_____\\/\\\\\\_____\\/\\\\\\/////////\\\\\\_\n" +
-//            "__\\///\\\\\\__________\\/\\\\\\_______\\/\\\\\\__/\\\\\\______\\//\\\\\\___\\///\\\\\\__________\\/\\\\\\_______\\/\\\\\\_\\/\\\\\\_______/\\\\\\______\\/\\\\\\_____\\/\\\\\\_______\\/\\\\\\_\n" +
-//            "____\\////\\\\\\\\\\\\\\\\\\_\\/\\\\\\_______\\/\\\\\\_\\///\\\\\\\\\\\\\\\\\\\\\\/______\\////\\\\\\\\\\\\\\\\\\_\\/\\\\\\_______\\/\\\\\\_\\/\\\\\\\\\\\\\\\\\\\\\\\\/____/\\\\\\\\\\\\\\\\\\\\\\_\\/\\\\\\_______\\/\\\\\\_\n" +
-//            "_______\\/////////__\\///________\\///____\\///////////___________\\/////////__\\///________\\///__\\////////////_____\\///////////__\\///________\\///__\n"
+//            " _____/\\\\\\////////____/\\\\\\\\\\\\\\\\\\\\\\\\\\____/\\\\\\/////////\\\\\\_____/\\\\\\////////____/\\\\\\\\\\\\\\\\\\\\\\\\\\__\\/\\\\\\////////\\\\\\__\\/////\\\\\\///____/\\\\\\\\\\\\\\\\\\\\\\\\\\__\n" +
+//            "  ___/\\\\\\/____________/\\\\\\/////////\\\\\\__\\//\\\\\\______\\///____/\\\\\\/____________/\\\\\\/////////\\\\\\_\\/\\\\\\______\\//\\\\\\_____\\/\\\\\\______/\\\\\\/////////\\\\\\_\n" +
+//            "   __/\\\\\\_____________\\/\\\\\\_______\\/\\\\\\___\\////\\\\\\__________/\\\\\\_____________\\/\\\\\\_______\\/\\\\\\_\\/\\\\\\_______\\/\\\\\\_____\\/\\\\\\_____\\/\\\\\\_______\\/\\\\\\_\n" +
+//            "    _\\/\\\\\\_____________\\/\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\______\\////\\\\\\______\\/\\\\\\_____________\\/\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\_\\/\\\\\\_______\\/\\\\\\_____\\/\\\\\\_____\\/\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\_\n" +
+//            "     _\\//\\\\\\____________\\/\\\\\\/////////\\\\\\_________\\////\\\\\\___\\//\\\\\\____________\\/\\\\\\/////////\\\\\\_\\/\\\\\\_______\\/\\\\\\_____\\/\\\\\\_____\\/\\\\\\/////////\\\\\\_\n" +
+//            "      __\\///\\\\\\__________\\/\\\\\\_______\\/\\\\\\__/\\\\\\______\\//\\\\\\___\\///\\\\\\__________\\/\\\\\\_______\\/\\\\\\_\\/\\\\\\_______/\\\\\\______\\/\\\\\\_____\\/\\\\\\_______\\/\\\\\\_\n" +
+//            "       ____\\////\\\\\\\\\\\\\\\\\\_\\/\\\\\\_______\\/\\\\\\_\\///\\\\\\\\\\\\\\\\\\\\\\/______\\////\\\\\\\\\\\\\\\\\\_\\/\\\\\\_______\\/\\\\\\_\\/\\\\\\\\\\\\\\\\\\\\\\\\/____/\\\\\\\\\\\\\\\\\\\\\\_\\/\\\\\\_______\\/\\\\\\_\n" +
+//            "        _______\\/////////__\\///________\\///____\\///////////___________\\/////////__\\///________\\///__\\////////////_____\\///////////__\\///________\\///__\n"
 //        );
+
         System.out.println(
                 """
                                   _____                    _____                    _____                    _____                    _____                    _____                    _____                    _____          \s
@@ -47,16 +49,12 @@ public class Output {
                                 \\::/    /                \\::/    /                \\::/    /                \\::/    /                \\::/    /                \\::/____/                \\::/    /                \\::/    /        \s
                                  \\/____/                  \\/____/                  \\/____/                  \\/____/                  \\/____/                  ~~                       \\/____/                  \\/____/         \s""".indent(1)
         );
-        try {
-            Thread.sleep(300);
-        } catch (InterruptedException ignored) {}
+		sleep(300);
 
         System.out.println("Welcome to Cascadia!");
 
         // another sleep call
-        try {
-            Thread.sleep(300);
-        } catch (InterruptedException ignored) {}
+		sleep(300);
     }
 
     public static void clearScreen() {
@@ -81,16 +79,49 @@ public class Output {
     	System.out.println("The current Habitat Tile + Wildlife Token pairs up for selection are: ");
     	for (HabitatTile i : tileTokenPairs.keySet()) {
     		System.out.println("Tile: " + i + ", Token: " + tileTokenPairs.get(i));
-    		printHalfTile(i);
+    		printHalfTile(i, ' ', ' ', ' ',' ');
     	}
     	System.out.println();
 	}
 
-	public static void displayTileMap(Player player) {
-		System.out.println("Player " + player.getPlayerName() + "'s current map of tiles are: \n");
-		for (HabitatTile tile : player.getPlayerTiles()) {
-			printHalfTile(tile);
+//	public static void displayTileMap(Player player) {
+//		System.out.println("Player " + player.getPlayerName() + "'s current map of tiles are: \n");
+//		for (HabitatTile tile : player.getPlayerTiles()) {
+//			printHalfTile(tile, ' ', ' ', ' ',' ');
+//		}
+//	}
+
+	// creating a new method for the starter habitat for now as this method will not be
+	// scalable.  Will create a scalable one later.
+	// TODO: Add actual token characters rather than the placeholders
+	public static void displayStarterHabitat(Player player) {
+		System.out.println("Player " + player.getPlayerName() + "'s current map of tiles are: ");
+//		for (HabitatTile tile : player.getPlayerTiles()) {
+//			printHalfTile(tile, ' ', ' ', ' ',' ');
+//		}
+		System.out.println(indentTile(player.getPlayerTiles().get(0).toFormattedString('A', 'B', 'C', 'D')));
+		System.out.println(combineTiles(player.getPlayerTiles().get(1).toFormattedString('A','B', 'C', 'D'),
+							player.getPlayerTiles().get(2).toFormattedString('A','B','C', 'D')) + "\n"
+		);
+	}
+
+	private static String indentTile(String tile) {
+		String[] lines = tile.split("\n");
+		// using an old for loop because I don't think changes in the new ones affect
+		// the thing it's iterating through
+		for (int i = 0; i < 4; i++) {
+			lines[i] = repeater(' ', 9) + lines[i];
 		}
+		return String.join("\n", lines);
+	}
+
+	private static String combineTiles(String first, String second) {
+		String[] firstLines = first.split("\n");
+		String[] secondLines = second.split("\n");
+		for (int i = 0; i < 4; i++) {
+			firstLines[i] += secondLines[i];
+		}
+		return String.join("\n", firstLines);
 	}
 	
 	public static void displayCommands() {
@@ -119,30 +150,42 @@ public class Output {
 			"\u001B[33m"
 	};
 
-//    public static void PrintFullTile (HabitatTile tile) {
-//        // NOTE: only draws the first habitat colour for now
-////        System.out.println(tile.habitat1.ordinal());
-//        System.out.println(HabitatColours[tile.getHabitat1().ordinal()] +
-//                "|||| |||| |||| ||||\n" +
-//                "||||           ||||\n" +
-//                "||||           ||||\n" +
-//                "|||| |||| |||| ||||\n" +
-//                ANSI_RESET
-//                );
-//    }
+	static String[] backgroundHabitatColours = {
+			"\u001B[42m",
+			"\u001B[46m",
+			"\u001B[44m",
+			"\u001B[47m",
+			"\u001B[43m"
+	};
 
-	// note that these printing things are all pretty primitive atm
-	public static void printHalfTile (HabitatTile tile) {
-		System.out.println(HabitatColours[tile.getHabitat1().ordinal()] +
-				"|||| |||| |||| ||||\n" +
-				"||||           ||||\n" +
-				ANSI_RESET +
-				HabitatColours[tile.getHabitat2().ordinal()] +
-				"||||           ||||\n" +
-				"|||| |||| |||| ||||\n" +
-				ANSI_RESET
+	// new display method which is closer to what he wants
+	// TODO: Change the background colour based on whether a token is selected
+	public static void printHalfTile (HabitatTile tile, char char1, char char2,
+									  char char3, char char4) {
+		String first = backgroundHabitatColours[tile.getHabitat1().ordinal()];
+		String second = backgroundHabitatColours[tile.getHabitat2().ordinal()];
+		String full =  "    |    |    |    " + ANSI_RESET + "\n";
+		System.out.println(
+				first + full +
+				first + "    |" + ANSI_RESET + "  " + char1 + "   " + char2 + "  " +
+				first  + "|    " + ANSI_RESET + "\n" +
+				second + "    |" + ANSI_RESET + "  " + char3  + "   " + char4 + "  " +
+				second  + "|    " + ANSI_RESET + "\n" +
+				second + full
 		);
 
+	}
 
+	// repeats character 'a' number times, just nicer than using a for loop
+	private static String repeater (char a, int number) {
+		char[] repeat = new char[number];
+		Arrays.fill(repeat, a);
+		return new String(repeat);
+	}
+
+	public static void sleep (int millis) {
+		try {
+			Thread.sleep(millis);
+		} catch (InterruptedException ignored) {}
 	}
 }
