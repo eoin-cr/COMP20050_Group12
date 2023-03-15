9e3a8f2962373028f1d11afda8aa5216d6bb7a2d
Eoin <79023828+eoin-cr@users.noreply.github.com>
2023-03-15 02:04:26 +0000

    Cleaned up bag code

--- src/main/java/cascadia/Bag.java
+++ src/main/java/cascadia/Bag.java
@@ -38,24 +38,42 @@ public class Bag {
      */
     // TODO: Change these values to the ones that correspond to the amount of players
     public static void makeBag (int numPlayers) {
-    	//make tokens
-    	remainingTokens.put(WildlifeToken.Bear, 30);
-    	remainingTokens.put(WildlifeToken.Elk, 30);
-        remainingTokens.put(WildlifeToken.Salmon, 30);
-        remainingTokens.put(WildlifeToken.Hawk, 30);
-        remainingTokens.put(WildlifeToken.Fox, 30);
-        
-        //make all possible tiles to draw from
-    	remainingHabitats.put(HabitatTile.Habitat.Forest, 20);
-        remainingHabitats.put(HabitatTile.Habitat.River, 20);
-        remainingHabitats.put(HabitatTile.Habitat.Wetland, 20);
-        remainingHabitats.put(HabitatTile.Habitat.Prairie, 20);
-        remainingHabitats.put(HabitatTile.Habitat.Mountain, 20);
+        putTokens();
+        putHabitats();
+        putKeystone();
+        generateMaxTiles(numPlayers);
+        createPossibleStarterTiles();
+    }
 
-        remainingTypes.put(HabitatTile.TileType.NON_KEYSTONE, 75);
-        remainingTypes.put(HabitatTile.TileType.KEYSTONE, 25);
-		
-    	
+    private static void createPossibleStarterTiles() {
+        // Creates the possible starter tiles, and adds them to a list.  The
+        // starter tiles are very specific (one keystone tile, at least one of each
+        // habitat, one tile with 2 possible tokens, one with 3, etc.) so we manually
+        // set most of the conditions (except the possible tokens on the tiles)
+        starterTileHelper(HabitatTile.Habitat.Wetland, HabitatTile.Habitat.River,
+                HabitatTile.Habitat.Forest, HabitatTile.Habitat.Prairie, HabitatTile.Habitat.Mountain);
+        starterTileHelper(HabitatTile.Habitat.Mountain, HabitatTile.Habitat.Forest,
+                HabitatTile.Habitat.Wetland, HabitatTile.Habitat.River, HabitatTile.Habitat.Prairie);
+        starterTileHelper(HabitatTile.Habitat.Forest, HabitatTile.Habitat.Mountain,
+                HabitatTile.Habitat.River, HabitatTile.Habitat.Wetland, HabitatTile.Habitat.Prairie);
+        starterTileHelper(HabitatTile.Habitat.River, HabitatTile.Habitat.Prairie,
+                HabitatTile.Habitat.Forest, HabitatTile.Habitat.Mountain, HabitatTile.Habitat.Wetland);
+        starterTileHelper(HabitatTile.Habitat.Prairie, HabitatTile.Habitat.Wetland,
+                HabitatTile.Habitat.River, HabitatTile.Habitat.Forest, HabitatTile.Habitat.Mountain);
+    }
+
+    private static void starterTileHelper(HabitatTile.Habitat one, HabitatTile.Habitat two,
+                                          HabitatTile.Habitat three, HabitatTile.Habitat four,
+                                          HabitatTile.Habitat five) {
+
+        starterTiles.add(new HabitatTile[]{
+                new HabitatTile(one, one, 1),
+                new HabitatTile(two, three, 3),
+                new HabitatTile(four, five, 2)
+        });
+    }
+
+    private static void generateMaxTiles(int numPlayers) {
         //make habitat tiles
         if (numPlayers == 2) {
         	setMaxTiles(43);
@@ -69,35 +87,29 @@ public class Bag {
         else {
         	throw new IllegalArgumentException("The number of players is not within the range 2-4.");
         }
-  
+    }
 
-        // Hard coding this is simpler than running a large amount of
-        // generations and checks in the starter habitat method.
-        starterTiles.add(new HabitatTile[]{
-                new HabitatTile(HabitatTile.Habitat.Wetland, HabitatTile.Habitat.Wetland, 1),
-                new HabitatTile(HabitatTile.Habitat.River, HabitatTile.Habitat.Forest, 3),
-                new HabitatTile(HabitatTile.Habitat.Prairie, HabitatTile.Habitat.Mountain, 2)
-        });
-        starterTiles.add(new HabitatTile[]{
-                new HabitatTile(HabitatTile.Habitat.Mountain, HabitatTile.Habitat.Mountain, 1),
-                new HabitatTile(HabitatTile.Habitat.Forest, HabitatTile.Habitat.Wetland, 3),
-                new HabitatTile(HabitatTile.Habitat.River, HabitatTile.Habitat.Prairie, 2)
-        });
-        starterTiles.add(new HabitatTile[]{
-                new HabitatTile(HabitatTile.Habitat.Forest, HabitatTile.Habitat.Forest, 1),
-                new HabitatTile(HabitatTile.Habitat.Mountain, HabitatTile.Habitat.River, 3),
-                new HabitatTile(HabitatTile.Habitat.Wetland, HabitatTile.Habitat.Prairie, 2)
-        });
-        starterTiles.add(new HabitatTile[]{
-                new HabitatTile(HabitatTile.Habitat.River, HabitatTile.Habitat.River, 1),
-                new HabitatTile(HabitatTile.Habitat.Prairie, HabitatTile.Habitat.Forest, 3),
-                new HabitatTile(HabitatTile.Habitat.Mountain, HabitatTile.Habitat.Wetland, 2)
-        });
-        starterTiles.add(new HabitatTile[]{
-                new HabitatTile(HabitatTile.Habitat.Prairie, HabitatTile.Habitat.Prairie, 1),
-                new HabitatTile(HabitatTile.Habitat.Wetland, HabitatTile.Habitat.River, 3),
-                new HabitatTile(HabitatTile.Habitat.Forest, HabitatTile.Habitat.Mountain, 2)
-        });
+    private static void putKeystone() {
+        remainingTypes.put(HabitatTile.TileType.NON_KEYSTONE, 75);
+        remainingTypes.put(HabitatTile.TileType.KEYSTONE, 25);
+    }
+
+    private static void putHabitats() {
+        //make all possible tiles to draw from
+        remainingHabitats.put(HabitatTile.Habitat.Forest, 20);
+        remainingHabitats.put(HabitatTile.Habitat.River, 20);
+        remainingHabitats.put(HabitatTile.Habitat.Wetland, 20);
+        remainingHabitats.put(HabitatTile.Habitat.Prairie, 20);
+        remainingHabitats.put(HabitatTile.Habitat.Mountain, 20);
+    }
+
+    private static void putTokens() {
+        //make tokens
+        remainingTokens.put(WildlifeToken.Bear, 30);
+        remainingTokens.put(WildlifeToken.Elk, 30);
+        remainingTokens.put(WildlifeToken.Salmon, 30);
+        remainingTokens.put(WildlifeToken.Hawk, 30);
+        remainingTokens.put(WildlifeToken.Fox, 30);
     }
 
 }
